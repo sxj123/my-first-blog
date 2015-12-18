2785d2ca05ddf073a29b3abe62585d8ed2a07ce5
sxj123 <1056017824@qq.com>
2015-12-18 10:51:44 +0800

    changed the html for the site

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
+
+PROJECT_DIR=dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0,PROJECT_DIR)
+
+os.environ["DJANGO_SETTINGS_MODULE"]= "mysite.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
 application = get_wsgi_application()
